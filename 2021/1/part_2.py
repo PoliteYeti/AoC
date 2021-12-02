9cb9c5b4d64940b119d32a58bff88acfdfed2a1e
James Hudson <james.hudson@boughtbymany.com>
2021-12-02 10:46:05 +0000

    Linting and tidying up. Answers add to readme.

--- 2021/1/part_2.py
+++ 2021/1/part_2.py
@@ -1,20 +1,23 @@
 import os
 
+
 def get_input():
-    with open(f'{os.path.dirname(__file__)}\\input.txt', 'r') as input_file:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as input_file:
         for line in input_file:
             yield int(line)
+
 
 def get_increases(items):
     last_item = None
     for current_item in items:
-        
+
         if not last_item:
             last_item = current_item
 
         if current_item > last_item:
             yield current_item
         last_item = current_item
+
 
 def get_windows(items):
     window = []
--- 2021/2/part_1.py
+++ 2021/2/part_1.py
@@ -1,14 +1,17 @@
 import os
 
+
 def get_input():
-    with open(f'{os.path.dirname(__file__)}\\input.txt', 'r') as input_file:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as input_file:
         for line in input_file:
             yield line.split()
-x = y = 0
 
-x = sum(int(item[1]) for item in get_input() if item[0] == 'forward')
-y = sum(int(item[1]) for item in get_input() if item[0] == 'down') + sum(-int(item[1]) for item in get_input() if item[0] == 'up')
 
-answer = x*y
+x = sum(int(item[1]) for item in get_input() if item[0] == "forward")
+y = sum(int(item[1]) for item in get_input() if item[0] == "down") - sum(
+    int(item[1]) for item in get_input() if item[0] == "up"
+)
+
+answer = x * y
 
 print(answer)
