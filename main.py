ec5cab126256e2069b7939a91c8b010c2109c2a7
James Hudson <james.hudson@boughtbymany.com>
2021-12-03 18:05:50 +0000

    Add argument parsing to main.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+import argparse
 import importlib
 import os
 
 from utils import get_input_factory
 
-YEAR = "2020"
-DAY = "01"
+YEAR = 2021
+DAY = 3
 
 
 def run_puzzle(year, day):
-    solution = importlib.import_module(f"year_{year}.day_{day}.solution")
+    solution = importlib.import_module(f"year_{year}.day_{day:02}.solution")
 
     input_file_path = os.path.join(
-        os.path.dirname(__file__), f"year_{year}", f"day_{day}", "input.txt"
+        os.path.dirname(__file__), f"year_{year}", f"day_{day:02}", "input.txt"
     )
 
     get_input = get_input_factory(input_file_path)
@@ -19,9 +20,18 @@ def run_puzzle(year, day):
     solution_1 = solution.part_1(get_input)
     solution_2 = solution.part_2(get_input)
 
-    print(f"{YEAR}, Day {DAY}, Part 1 solution is: {solution_1}")
-    print(f"{YEAR}, Day {DAY}, Part 2 solution is: {solution_2}")
+    print(f"{year}, Day {day:02}, Part 1 solution is: {solution_1}")
+    print(f"{year}, Day {day:02}, Part 2 solution is: {solution_2}")
 
 
 if __name__ == "__main__":
-    run_puzzle(YEAR, DAY)
+    parser = argparse.ArgumentParser(
+        prog="AoC Runner",
+        description="Handy dandy util for executing puzzle solutions.",
+    )
+    parser.add_argument("--day", action="store", default=DAY, type=int)
+    parser.add_argument("--year", action="store", default=YEAR, type=int)
+
+    arguments = parser.parse_args()
+
+    run_puzzle(arguments.year, arguments.day)
